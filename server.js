c28c14c6a263c2c5cf60cc32f7d8e951e6f8900e
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-03-24 07:29:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@
             const {connection}=require("./database/db")
 
     app.get("/", (req, res) => {
-    res.send("<style>h1 {position: fixed; top: 50%; left: 50%; transform: translate(-50%, -70%); text-align: center; color: red; font-weight: bolder; text-transform: uppercase; font-size: 60px;}</style><h1>hii i am backend developer</h1>");
+    res.send("<style>h1 {position: fixed; top: 50%; left: 50%; transform: translate(-50%, -70%); text-align: center; color: red; font-weight: bolder; text-transform: uppercase; font-size: 60px;}</style><h1>hii i am sourav
+                 welcome to my REST API JSON DATA 
+             </h1>");
 });
 
 
