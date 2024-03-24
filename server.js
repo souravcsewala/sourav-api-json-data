73cc36b7d6804463a108ba6b64e1c7b2c448c0a1
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-03-24 07:47:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,11 +24,21 @@ app.get("/", (req, res) => {
                         text-transform: uppercase;
                         font-size: 60px;
                     }
+                    a{
+                        position: fixed;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -40%);
+                        text-align: center;
+                        font-weight: bolder;
+                        text-transform: uppercase;
+                        font-size: 60px;
+                     }
                 </style>
             </head>
             <body>
                 <h1 style="color: red;">hii i am sourav ENGINEER WALA</h1>
-                <h1 style="color:green;">welcome to my REST API JSON DATA</h1>
+                <h2 style="color:green;">welcome to my REST API JSON DATA</h2>
                 <a href="https://sourav-api-json-data.onrender.com/api/data/product-test-api" style="color:blue;">click here</a>
             </body>
         </html>
