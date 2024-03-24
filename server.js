4149a832ecf67ac3d236bad94b791ebb6a50e25c
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-03-24 07:34:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,35 @@
             const pruductRoute=require("./routes/productroute");
             const {connection}=require("./database/db")
 
-    app.get("/", (req, res) => {
-    res.send("<style>h1 {position: fixed; top: 50%; left: 50%; transform: translate(-50%, -70%); text-align: center; color: red; font-weight: bolder; text-transform: uppercase; font-size: 60px;}</style><h1>hii i am sourav
-                 welcome to my REST API JSON DATA 
-             </h1>");
+   
+app.get("/", (req, res) => {
+    const htmlContent = `
+        <html>
+            <head>
+                <style>
+                    h1 {
+                        position: fixed;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -70%);
+                        text-align: center;
+                        color: red;
+                        font-weight: bolder;
+                        text-transform: uppercase;
+                        font-size: 60px;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>hii i am sourav
+                    welcome to my REST API JSON DATA 
+                </h1>
+            </body>
+        </html>
+    `;
+    res.send(htmlContent);
 });
+
 
 
 
